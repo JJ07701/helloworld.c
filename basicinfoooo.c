973ed6264d41ffea23ba53ce932c8293df9cbb26
JJ07701 <jangraj845@gmail.com>
2025-08-13 16:33:38 +0530

    Add files via upload

---
+++ basicinfoooo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int name;
+    printf("enter yourt name\n");
+    scanf("%d",&name);
+    int batch;
+    printf("enter your batch\n");
+    scanf("%d",&batch);
+    
+    return 0;
+}
